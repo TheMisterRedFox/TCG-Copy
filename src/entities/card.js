2b63ce04538e00291e66c99a3477fb928958f97f
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2025-11-19 12:56:14 +0000

    fix: code style and lint code

--- src/entities/card.js
+++ src/entities/card.js
@@ -1,31 +1,31 @@
 export class Card {
-    constructor(id, name, rarity) {
-        this.id = id;
-        this.name = name;
-        this.rarity = rarity;
-    }
+	constructor(id, name, rarity) {
+		this.id = id;
+		this.name = name;
+		this.rarity = rarity;
+	}
 
-    getId() {
-        return this.id;
-    }
+	getId() {
+		return this.id;
+	}
 
-    setId(id) {
-        this.id = id;
-    }
+	setId(id) {
+		this.id = id;
+	}
 
-    getName() {
-        return this.name;
-    }
+	getName() {
+		return this.name;
+	}
 
-    setName(name) {
-        this.name = name;
-    }
+	setName(name) {
+		this.name = name;
+	}
 
-    getRarity() {
-        return this.rarity;
-    }
+	getRarity() {
+		return this.rarity;
+	}
 
-    setRarity(rarity) {
-        this.rarity = rarity;
-    }
+	setRarity(rarity) {
+		this.rarity = rarity;
+	}
 }
