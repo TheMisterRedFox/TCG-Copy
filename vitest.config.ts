b6505aad8bbbe8451e86ecd4eef565d49a84c93b
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2025-11-20 12:56:39 +0000

    ci: update coverage reporting configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default mergeConfig(
 			root: fileURLToPath(new URL('./', import.meta.url)),
 			coverage: {
 				provider: 'v8',
-				reporter: ['text', 'json', 'json-summary', 'lcov'],
+				reporter: ['text', 'json', 'json-summary'],
 				reportsDirectory: './coverage',
 				reportOnFailure: true,
 			},
