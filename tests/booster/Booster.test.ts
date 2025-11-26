17b06c5683a03f9f36e520b863858ade422b57e0
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2025-11-26 12:52:52 +0000

    tests: move tests to it's own folder and add new tests for component

--- tests/booster/Booster.test.ts
+++ tests/booster/Booster.test.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
-import Booster from './Booster.vue';
+import Booster from '@/components/booster/Booster.vue';
 
 describe('Booster.vue', () => {
 	it('mounts without errors', () => {
