ad494c32141773ee6d567a8ff32636a368766117
RetrowPoLo <100202418+RetrowPoLo@users.noreply.github.com>
2025-11-28 13:36:44 +0000

    feat: add new type for "v2"

--- src/interfaces/GeneralTypes.ts
+++ src/interfaces/GeneralTypes.ts
@@ -24,3 +24,38 @@ export interface Stat {
 	base_stat: number;
 	stat: { name: string };
 }
+
+/**
+ * ------------------------------------
+ * V2
+ * ------------------------------------
+ */
+
+export interface CardV2 {
+	category: string;
+	id: string;
+	image: string;
+	localId: string;
+	name: string;
+	rarity: string;
+	hp: number;
+	types: string[];
+	evolveFrom: string;
+	description: string;
+	stage: string;
+	attacks: AttackV2[];
+	weaknesses: WeaknessV2[];
+	retreat: number;
+}
+
+export interface AttackV2 {
+	cost: string[];
+	damage: number | null;
+	effect: string;
+	name: string;
+}
+
+export interface WeaknessV2 {
+	type: string;
+	value: string;
+}
